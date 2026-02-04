7657c7b741f93027e81e7e5aae51ed52734c7ab4
elyan-becavin <drive.elyan@gmail.com>
2026-02-04 15:41:33 +0100

    Rename PlayerTeam to EnemyTeam
    Refactor PlayerTeam to EnemyTeam with new attributes.

--- EnemyTeam.py
+++ EnemyTeam.py
@@ -1,14 +1,11 @@
 import Team
 
-class PlayerTeam(Team):
-    def __init__(self, members, nb_warriors, nb_hunters, nb_wizards, damage, loot, flee):
+class EnemyTeam(Team):
+    def __init__(self, members, unit_type, damage, loot):
         super().__init__(members)
-        self.__nb_warriors = nb_warriors
-        self.__nb_hunters = nb_hunters
-        self.__nb_wizards = nb_wizards
+        self.__unit = unit_type
         self.__damage = damage
         self.__loot = loot
-        self.__flee = flee
     
     def get_damage(self):
         return self.__damage
@@ -16,18 +13,5 @@ class PlayerTeam(Team):
     def get_loot(self):
         return self.__loot
     
-    def get_flee(self):
-        return self.__flee
-    
-    def get_nb_warriors(self):
-        return self.__nb_warriors
-    
-    def get_nb_hunters(self):
-        return self.__nb_hunters
-    
-    def get_nb_wizards(self):
-        return self.__nb_wizards
-    
-    def __repr__(self):
-        return (f"PlayerTeam(Warriors={self.__nb_warriors}, Hunters={self.__nb_hunters}, "
-                f"Wizards={self.__nb_wizards}, Damage={self.__damage}, Loot={self.__loot}, Flee={self.__flee})")
+    def get_unit_type(self):
+        return self.__unit
