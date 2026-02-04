3816d0d4da8114417e49db2cc4243d7315f8d88c
Kahina <0kahinalarkem@gmail.com>
2026-02-04 14:17:10 +0100

    feat: add Wizard class closes #3

---
+++ models.py
@@ -0,0 +1,9 @@
+
+class Wizard:
+    def __init__(self):
+        self.degat = random.choice([2, 3, 4])
+        self.chance = 20
+        self.fuite = 10
+        self.prix = 15
+        self.type_unite = 'wizard'
+
