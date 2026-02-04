56d74c141f0752464a426be2c276cbfede0627da
Kahina <0kahinalarkem@gmail.com>
2026-02-04 14:25:14 +0100

    feat: add Zombie class closes #6

--- models.py
+++ models.py
@@ -7,3 +7,9 @@ class Wizard:
         self.prix = 15
         self.type_unite = 'wizard'
 
+
+class Zombie:
+    def __init__(self):
+        self.degat = random.choice([1, 2])
+        self.loot = random.choice([0.5, 1])
+
